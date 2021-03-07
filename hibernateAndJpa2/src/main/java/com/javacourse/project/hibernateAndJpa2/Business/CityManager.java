0a837f75d4744100ac6b7296d66374c767425f47
SkymanSky <79801786+SkymanSky@users.noreply.github.com>
2021-03-07 23:30:11 +0300

    Add files via upload
    First upload of hibernateAndJpa2

---
+++ hibernateAndJpa2/src/main/java/com/javacourse/project/hibernateAndJpa2/Business/CityManager.java
@@ -0,0 +1,63 @@
+package com.javacourse.project.hibernateAndJpa2.Business;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.javacourse.project.hibernateAndJpa2.DataAccess.ICityDal;
+import com.javacourse.project.hibernateAndJpa2.Entities.City;
+
+@Service
+public class CityManager implements ICityService {
+	
+	private ICityDal cityDal;
+	
+	@Autowired
+	
+	public CityManager(ICityDal cityDal) {
+		
+		this.cityDal = cityDal;
+	}
+
+	@Override
+	@Transactional
+	public List<City> getAll() {
+		
+		return this.cityDal.getAll();
+	}
+
+	@Override
+	@Transactional
+	public void add(City city) {
+		//business dediğimiz iş kuralları yazılır. Buralarda çeşitli
+		//kodlar olur. Burası müşteri isteklerine göre şekillenir.
+		this.cityDal.add(city);
+		
+	}
+
+	@Override
+	@Transactional
+	public void update(City city) {
+		 
+		this.cityDal.update(city);
+	}
+
+	@Override
+	@Transactional
+	public void delete(City city) {
+		 
+		this.cityDal.delete(city);
+		
+	}
+
+	@Override
+	@Transactional
+	public City getById(int id) {
+		
+		return this.cityDal.getById(id);
+	}
+	
+	
+}
